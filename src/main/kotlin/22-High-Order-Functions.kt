353b555287fc7159b74df5d8e0bd66b6ab6e6b86
Nguyễn Hoàng Ân <an2419998@gmail.com>
2020-12-13 20:45:39 +0700

    learning High Order function

---
+++ src/main/kotlin/22-High-Order-Functions.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var result = calculate(1, 2, ::execute)
+    println(result)
+
+    var result1 = calculate(3, 4, fun(x, y: Int): Int = x + y)
+    println(result1)
+}
+
+fun calculate(x: Int, y: Int, func: (Int, Int) -> Int): Int {
+    return func(x, y)
+}
+
+fun execute(x: Int, y: Int) = x + y
