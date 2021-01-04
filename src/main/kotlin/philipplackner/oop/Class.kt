fa221f9fa216260fd1e07fda15d2019a74963d34
Nguyễn Hoàng Ân <an2419998@gmail.com>
2021-01-04 15:35:03 +0700

    Practice classes

---
+++ src/main/kotlin/philipplackner/oop/Class.kt
@@ -0,0 +1,12 @@
+package philipplackner.oop
+
+fun main() {
+    val rec = Rectangle(4.0, 7.0)
+    println("Rectangle area is ${rec.area()}")
+    println("Rectangle perimeter is ${rec.perimeter()}")
+    println("Is rectangle a square? ${rec.isSquare()}")
+
+    val circle = Circle(5.0)
+    val circle2 = Circle(7.0)
+}
+
